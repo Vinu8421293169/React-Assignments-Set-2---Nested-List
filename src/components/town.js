2916dd403c719c9df5d2c6c38f51a5ddc25dec2c
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-12-06 01:04:29 +0530

    commit

--- src/components/town.js
+++ src/components/town.js
@@ -3,7 +3,7 @@ export default function Town({ towns }) {
   return (
     <>
       {towns.map((ele, index) => {
-        return <div>{ele.name}</div>;
+        return <div id={`town${index + 1}`}>{ele.name}</div>;
       })}
     </>
   );
